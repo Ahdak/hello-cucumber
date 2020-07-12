a9cdf4fe42d68beb1ac7eec5d2bfbc65d23163fb
Ahmed DAMMAK <ahmed.dammak.net@gmail.com>
2020-07-12 17:52:57 +0200

    Use Cucumber Junit Jupiter
    Signed-off-by: Ahmed DAMMAK <ahmed.dammak.net@gmail.com>

--- src/test/java/net/dammak/hellocucumber/CucumberTestRunner.java
+++ src/test/java/net/dammak/hellocucumber/CucumberTestRunner.java
@@ -1,14 +1,11 @@
 package net.dammak.hellocucumber;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.platform.engine.Cucumber;
 import io.cucumber.spring.CucumberContextConfiguration;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "classpath:features", plugin = {"pretty", "json:target/cucumber-report.json"})
 @CucumberContextConfiguration
 @SpringBootTest()
+@Cucumber
 public class CucumberTestRunner {
 }
